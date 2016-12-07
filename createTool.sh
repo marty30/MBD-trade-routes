1c55512dadf11ca010138665f62fedc01a152b27
Robin Aly <robin@aly.de>
2016-12-07 09:42:54 +0100

    Create shaded jar, improved tool creation

--- createTool.sh
+++ createTool.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 TOOL=$1
 shift
-TOOLTEST="${TOOL}Test"
-echo $TOOL >> src/main/resources/tools.txt
-sed -e "s/object Template/object $TOOL/g" src/main/scala/nl/utwente/bigdata/Template.scala > src/main/java/nl/utwente/bigdata/$TOOL.scala
+sed -e "s/object Template/object $TOOL/g" src/main/scala/nl/utwente/bigdata/Template.scala > src/main/scala/nl/utwente/bigdata/$TOOL.scala
+echo "Created template for $TOOL, you can now edit the file: src/main/scala/nl/utwente/bigdata/$TOOL.scala"
