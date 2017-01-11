c6e6946949a41f1f46cfd25a9baffd7f67afb633
Robin Aly <robin@aly.de>
2017-01-11 15:12:52 +0100

    Corrected spelling mistake in SparkConf

--- templates/python/Template.py
+++ templates/python/Template.py
@@ -12,12 +12,11 @@ def main():
   # parse arguments 
   in_dir, out_dir = sys.argv[1:]
   
-  conf = pyspark.SparkConnf().setAppName("$toolName %s %s" % (in_dir, out_dir))
+  conf = pyspark.SparkConf().setAppName("%s %s %s" % (os.path.basename(__file__), in_dir, out_dir))
   sc = pyspark.SparkContext(conf=conf)
   
   # invoke job and put into output directory
   doJob(sc.textFile(in_dir)).saveAsTextFile(out_dir)
-
 
 if __name__ == '__main__':
   main()
